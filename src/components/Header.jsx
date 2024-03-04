3db63734bec45d863ad4d27419d1a8323cc43129
bexy65 <naimertan65@gmail.com>
2024-03-04 21:08:16 +0200

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <p>My Store</p>
+      <nav>
+        <ul>
+          <li>Category1</li>
+          <li>Category2</li>
+          <li>Category3</li>
+          <li>Category4</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
