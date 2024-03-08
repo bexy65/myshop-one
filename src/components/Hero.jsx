21eb61932f504d4d5ce208904f1130597ff12f0f
bexy65 <naimertan65@gmail.com>
2024-03-08 17:57:45 +0200

    inital css fix, hero cover position and structure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div className='hero w-full text-center p-11 bg-blue-300'>
-        <div className='hero-cover p-4 rounded-xl'>
-            <h1 className='my-10 text-white'>HIGH QUALITY PRODUCTS</h1>
-            <button className='my-10 rounded-xl bg-red-300 p-3 text-white'>Shop Now</button>
-        </div>
+    <div className="hero w-full text-center bg-blue-300">
+      <div className="hero-cover text-lg p-2 md:p-4 lg:p-6">
+        <h1 className="my-5 md:my-8 lg:my-10 text-white">
+          HIGH QUALITY PRODUCTS
+        </h1>
+        <button className="my-10 rounded-xl bg-red-400 hover:bg-red-300 p-3 text-white">
+          Shop Now
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
