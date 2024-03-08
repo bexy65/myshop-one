0a0ed2de4b2066a686712baa169ee184d5c065a2
bexy65 <naimertan65@gmail.com>
2024-03-08 14:57:53 +0200

    basic hero structure, add button and header text for hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='hero w-full text-center p-11 bg-blue-300'>
+        <div className='hero-cover p-4 rounded-xl'>
+            <h1 className='my-10 text-white'>HIGH QUALITY PRODUCTS</h1>
+            <button className='my-10 rounded-xl bg-red-300 p-3 text-white'>Shop Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
