f72229cbfea855dc6eda99250354441595c1b525
bexy65 <106264629+bexy65@users.noreply.github.com>
2024-03-14 15:22:55 +0200

    refactor code, change ui for header with category, add transition to sidenav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Categories from "./components/Categories";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <Categories />
       <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo magnam
         exercitationem ducimus, iusto cumque, aut laudantium voluptates unde
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import HamburgerNavIcon from "./HamburgerNavIcon";
+import Categories from "./Categories";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const [expanded, setExpanded] = useState(false);
+
+  function toggleCategoryListWidth() {
+    setExpanded(!expanded);
+  }
+
   return (
-    <div className="header">
-      <HamburgerNavIcon
-        icon={<FontAwesomeIcon className="text-2xl" icon={faBars} />}
-      />
-      <h2>MY STORE</h2>
-      <div className="header-button-container">
-        <button>
-          <FontAwesomeIcon className="text-xl" icon={faUser} />
-        </button>
-        <div className="header-button-divider"></div>
-        <button>
-          <FontAwesomeIcon className="text-xl" icon={faShoppingCart} />
-        </button>
+    <div className="header-container">
+      <div className="header" onClick={toggleCategoryListWidth}>
+        <HamburgerNavIcon
+          icon={<FontAwesomeIcon className="text-2xl" icon={faBars} />}
+        />
+        <h2>MY STORE</h2>
+        <div className="header-button-container">
+          <button>
+            <FontAwesomeIcon className="text-xl" icon={faUser} />
+          </button>
+          <div className="header-button-divider"></div>
+          <button>
+            <FontAwesomeIcon className="text-xl" icon={faShoppingCart} />
+          </button>
+        </div>
       </div>
+      <Categories state={expanded} function={toggleCategoryListWidth} />
     </div>
   );
 }
