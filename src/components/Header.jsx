ae1e93017ce0fd06e31c18daa76a40ca88a5f5f6
bexy65 <106264629+bexy65@users.noreply.github.com>
2024-03-14 13:57:29 +0200

    tweak the button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import HamburgerNavIcon from './HamburgerNavIcon';
+import React from "react";
+import HamburgerNavIcon from "./HamburgerNavIcon";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
-    <div className='header'>
-      <HamburgerNavIcon />
+    <div className="header">
+      <HamburgerNavIcon
+        icon={<FontAwesomeIcon className="text-2xl" icon={faBars} />}
+      />
       <h2>MY STORE</h2>
-      <div className='header-button-container'>
+      <div className="header-button-container">
         <button>
-          <FontAwesomeIcon className='text-xl' icon={faUser} />
+          <FontAwesomeIcon className="text-xl" icon={faUser} />
         </button>
-        <div className='header-button-divider'></div>
+        <div className="header-button-divider"></div>
         <button>
-          <FontAwesomeIcon className='text-xl' icon={faShoppingCart} />
+          <FontAwesomeIcon className="text-xl" icon={faShoppingCart} />
         </button>
       </div>
     </div>
