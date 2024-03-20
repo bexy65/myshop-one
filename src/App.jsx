dc648b1916c09480c05c76bb11b5497cfaff1f6d
bexy65 <106264629+bexy65@users.noreply.github.com>
2024-03-20 12:54:18 +0200

    add state to selected category

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import ProductGrid from "./components/ProductGrid";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
   return (
     <>
-      <Header />
+      <Header setSelectedCategory={setSelectedCategory} />
       <Hero />
+      <ProductGrid selectedCategory={selectedCategory} />
       <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo magnam
         exercitationem ducimus, iusto cumque, aut laudantium voluptates unde
