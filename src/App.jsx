98903933016ea452e5bfc7bd25775c0e447c55ca
bexy65 <106264629+bexy65@users.noreply.github.com>
2024-03-21 13:23:09 +0200

    better imports refactor imports and structure of fs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import ProductGrid from "./components/ProductGrid";
 import MainContainer from "./components/MainContainer";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Header setSelectedCategory={setSelectedCategory} />
       <Hero />
       <MainContainer
-        ProductGrid={ProductGrid}
         selectedCategory={selectedCategory}
         tag="div"
         className="main-container"
