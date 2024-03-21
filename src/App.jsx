efca4892397f688b4f568b6ca2ecef16a9b9054c
bexy65 <106264629+bexy65@users.noreply.github.com>
2024-03-21 13:22:50 +0200

    refactor and created filter saction with main container

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import ProductGrid from "./components/ProductGrid";
+import MainContainer from "./components/MainContainer";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -11,7 +12,12 @@ function App() {
     <>
       <Header setSelectedCategory={setSelectedCategory} />
       <Hero />
-      <ProductGrid selectedCategory={selectedCategory} />
+      <MainContainer
+        ProductGrid={ProductGrid}
+        selectedCategory={selectedCategory}
+        tag="div"
+        className="main-container"
+      />
     </>
   );
 }
